2e04b6142d360e84a1b0b29aebb662539b194ab3
daniel <daniel@thedaniweb.eu.org>
2024-05-20 16:48:02 -0400

    asdasd

--- src/components/products/composables/useSuppliers.ts
+++ src/components/products/composables/useSuppliers.ts
@@ -1,6 +1,14 @@
+import type { Tables } from "@/lib/database.types"
+import { ref } from "vue"
 
 
 
 export const useSupplier = () => {
-    const suppliers = ref
+    const suppliers = ref<Tables<'supplier'>[]>([])
+
+    const error = ref<string | null>(null)
+
+    const fetchProductsBySupplier = async (supplierid: Tables<'supplier'>) => {
+
+    }
 }
